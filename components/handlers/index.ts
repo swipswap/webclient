fbe1ab147572d5e53b277f00303da204be3bfaf5
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2020-10-27 16:45:57 +0700

    added approve function

--- components/handlers/index.ts
+++ components/handlers/index.ts
@@ -1,12 +1,37 @@
+import { tokenABI, erc20TokenABI } from "../../scripts/ABIs"
 import { ethAPI } from "../../scripts/ethereum"
+import { PoolState } from '../typeDefinitions'
 
 export const handleChange = (setState) => e => {
     const {name, value} = e.target
     setState(prevState => ({...prevState, [name]: value}))
 }
 
-export const handleApprove = (state) => e => {
-    console.log(state)
+export const handleApprove = (state: PoolState) => async e => {
+    const { address, amount } = state
+    let ABI = erc20TokenABI
+    let tx
+    let poolAddress = '0x4b03Ac42133936ABf9c7C26c0aF3a08C27d56182'
+    const contract = await ethAPI.contractInterface({ contractAddress: address, contractABI: ABI })
+    const { allowance, decimals } = await ethAPI.checkAllowance({ poolAddress, contract })
+    const tokenBalance = await ethAPI.getTokenBalance({ contract })
+    const value = amount * (10 ** decimals)
+
+    if (Number(tokenBalance) < Number(value)) {
+        alert('Insufficient Funds')
+    } 
+    
+    if (Number(allowance) <= Number(value)) {
+        try {
+            tx = await contract.approve('0x4b03Ac42133936ABf9c7C26c0aF3a08C27d56182', value)
+            tx = await tx.wait()
+            alert(tx.transactionHash)
+        }catch(err){
+            console.log(err)
+        }
+    } else {
+        alert(`You have already approved ${allowance / (10 ** decimals)}`)
+    }
 }
 
 export const handleSubmitPool = (state) => e => {
