ace86946a18ee1a0f7b15224f1100c6646a7b5fa
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2020-11-26 02:31:17 +0700

    increased height

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,7 +3,7 @@ import SocialLinks from "../SocialLinks";
 export default function Footer({ lightTheme }){
   const bgMode = !lightTheme ? 'bg-swip-deep' : ''
   return(
-    <div className={`p-5 h-1/5 text-center mx-auto ${bgMode}`}>
+    <div className={`p-5 h-1/6 text-center mx-auto ${bgMode}`}>
       <SocialLinks lightTheme={lightTheme} />
     </div>
   )
