538dd5128deeec02110c1318e160562f80e24361
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2020-11-26 02:33:00 +0700

    changed from theme to lightTheme and other tailwind fixes

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,21 +1,21 @@
 import useBaseURL from "../hooks/useBaseURL"
 
 // const heroText = "Exchange digital currencies between peers without any central authority. An ethereum smart contract serves as the escrow."
-export default function Hero({ theme, children }){
+export default function Hero({ lightTheme, children }){
     const {baseURL} = useBaseURL()
 
     const isENS = baseURL
-    const isLight = theme === true
+    const isLight = lightTheme === true
     return (
-        <section className={`w-full 
+        <section className={`
             ${isENS && isLight ? "bg-ens-hero-light" : // If on ENS and its light mode, render light
             isENS && !isLight ? 'bg-ens-hero-dark' : // If on ENS and on dark mode, render dark
             !isENS && isLight ? 'bg-hero-light' : // Not on ENS but is light
            'bg-hero-dark'  // Render dark in any other condition
-            } bg-hero-bg p-10 bg-no-repeat bg-cover bg-center tablets:bg-cover tablets:bg-center tablets:h-hero-big`}>
-            <div className='pt-8 bg-right-bottom text-white w-hero-txt h-auto tablets:w-hero-big tablets:px-20 md:w-3/6 md:pt-24 mx-auto'>
+            } pt-10 bg-hero-bg bg-no-repeat bg-cover bg-center tablets:bg-cover tablets:bg-center h-5/6 transition duration-1000 ease-in-out h-full`}>
+            {/* <div className='pt-6 tablets:pt-8 mb-3 bg-right-bottom text-white w-hero-txt h-auto tablets:w-hero-big tablets:px-20 md:w-3/6 md:pt-24 mx-auto'> */}
               {children}
-            </div>
+            {/* </div> */}
             
         </section>
     )
